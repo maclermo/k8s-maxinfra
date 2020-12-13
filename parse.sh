40f9281bb50b0c94dc84783b3e364eaba3b8c918
Maxime Clermont <maxime.clermont@corp.ovh.com>
2020-12-13 18:51:22 -0500

    OK

--- parse.sh
+++ parse.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-docker build -t "k8s-maxinfra/$1" /kube/k8s-maxinfra/deployments/$1/.
-kubectl delete -f "/kube/k8s-maxinfra/definitions/$1.yaml"
-kubectl apply -f "/kube/k8s-maxinfra/definitions/$1.yaml"
+docker image rm k8s-maxinfra/$1
+docker build -t k8s-maxinfra/$1 /kube/k8s-maxinfra/deployments/$1/.
+kubectl delete -f /kube/k8s-maxinfra/definitions/$1.yaml
+kubectl apply -f /kube/k8s-maxinfra/definitions/$1.yaml
